7d8db22cc944e73281dd369a61402bcb782cafe4
LipCoding <sb921204@naver.com>
2021-12-01 17:51:25 +0900

    이펙티브 자바 공부
    item8 ~ 11

---
+++ EffectiveJava/CommonMethod/item10/PhoneNumber.java
@@ -0,0 +1,31 @@
+package CommonMethod.item10;
+
+public class PhoneNumber {
+    private final short areaCode, prefix, lineNum;
+
+    public PhoneNumber(int areaCode, int prefix, int lineNum) {
+        this.areaCode = rangeCheck(areaCode, 999, "지역코드");
+        this.prefix = rangeCheck(prefix, 999, "프리픽스");
+        this.lineNum = rangeCheck(lineNum, 999, "가입자 번호");
+    }
+
+    private static short rangeCheck(int val, int max, String arg) {
+        if(val < 0 || val > max) 
+            throw new IllegalArgumentException(arg + ": " + val);
+        return (short)val;
+    }
+
+    @Override public boolean equals(Object o) {
+        if(o == this)                       // 일관성
+            return true;
+        if(!(o instanceof PhoneNumber))     // 대칭성
+            return false;
+        PhoneNumber pn = (PhoneNumber)o;
+        
+        // 동치성 검사
+        return pn.lineNum == lineNum && pn.prefix == prefix
+                && pn.areaCode == areaCode; 
+    }
+}
+
+
